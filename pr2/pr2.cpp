8ad4c23d86dabd9dc922ed07315de602fc8821d6
Your Name <you@example.com>
2026-09-21 23:30:18 +0300

    first main_02 commit

--- pr2/pr2.cpp
+++ pr2/pr2.cpp
@@ -6,11 +6,11 @@ int main()
 	double z1; // результат обчислення 1-го виразу
 	// double z2; // результат обчислення 2-го виразу
 	cout << "x = "; cin >> x;
-	z1 = 2 * x;
-	// z2 = x + x;
+	//z1 = 2 * x;
+		z2 = x + x;
 	cout << endl;
-	cout << "z1 = " << z1 << endl;
-	// cout << "z2 = " << z2 << endl;
+	// cout << "z1 = " << z1 << endl;
+		cout << "z2 = " << z2 << endl;
 	cin.get();
 	return 0;
 }
